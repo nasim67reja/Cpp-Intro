f5ea489b3cd65bc96e4e1c2c647e02c4ed2a83fa
Nasim Reja <nasimreja67@gmail.com>
2022-11-25 17:23:52 +0600

    A little Update

--- main.cpp
+++ main.cpp
@@ -30,34 +30,65 @@ using namespace std;
 
 /////////// 👉👉👉👉👉 C++ String
 
+// int main()
+// {
+//     string firstName = "Nasim";
+//     string lastName = "Reja";
+//     // cout << firstName + " " + lastName << endl; // Concat string
+//     // string fullName = firstName.append(lastName);
+//     // cout << fullName << endl;
+
+//     string x = "10";
+//     int y = 20;
+//     // string z=x+y; // it will not work here
+//     // 🖐 String length
+//     string txt = "Lasim Reja";
+//     // cout << txt.length() << endl; // or txt.size()
+
+//     // 🖐 Change String Characters
+//     txt[0] = 'N';
+//     // cout << txt << endl;
+//     string specialChar = "We are the so-called \" Vikings \" from the \tnorth.";
+//     // cout << specialChar << endl;
+
+//     // 🖐 C++ User Input Strings
+
+//     string fullName;
+//     cout << "Type Your Fullname here : " << endl;
+//     // cin >> fullName;
+//     // cout << fullName; // there is a problem if you type Nasim Reja it will give only Nasim the text after space character will not be shown
+//     // getline(cin, fullName);
+//     // cout << "Your Full Name is : " << fullName << endl;
+//     cout << min(5, 10);
+//     return 0;
+// }
+
+// 👉👉👉 loop, if..else
+
 int main()
 {
-    string firstName = "Nasim";
-    string lastName = "Reja";
-    // cout << firstName + " " + lastName << endl; // Concat string
-    // string fullName = firstName.append(lastName);
-    // cout << fullName << endl;
+    int myAge = 0;
+    if (myAge || false)
+    {
+        cout << "You are a Voter !" << endl;
+    }
+    else
+    {
+        cout << "You are not a Voter !" << endl;
+    }
 
-    string x = "10";
-    int y = 20;
-    // string z=x+y; // it will not work here
-    // 🖐 String length
-    string txt = "Lasim Reja";
-    // cout << txt.length() << endl; // or txt.size()
+    //  for loop
 
-    // 🖐 Change String Characters
-    txt[0] = 'N';
-    // cout << txt << endl;
-    string specialChar = "We are the so-called \" Vikings \" from the \tnorth.";
-    // cout << specialChar << endl;
+    for (int i = 1; i <= 2; ++i)
+    {
+        cout << "Outer: " << i << "\n"; // Executes 2 times
 
-    // 🖐 C++ User Input Strings
+        // Inner loop
+        for (int j = 1; j <= 3; ++j)
+        {
+            cout << " Inner: " << j << "\n"; // Executes 6 times (2 * 3)
+        }
+    }
 
-    string fullName;
-    cout << "Type Your Fullname here : " << endl;
-    // cin >> fullName;
-    // cout << fullName; // there is a problem if you type Nasim Reja it will give only Nasim the text after space character will not be shown
-    getline(cin, fullName);
-    cout << "Your Full Name is : " << fullName << endl;
     return 0;
 }
