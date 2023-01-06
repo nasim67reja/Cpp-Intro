ebfbc01e8097acd94dbb7dd4de692da876e6e8b3
Nasim Reja <nasimreja67@gmail.com>
2023-01-06 11:53:09 +0600

    Binary Search Problem B

--- C++ STL/Pair and Vector,Iterators/Binary.cpp
+++ C++ STL/Pair and Vector,Iterators/Binary.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <utility>
@@ -7,55 +8,105 @@
 
 using namespace std;
 
+// //////// 👉👉👉👉👉👉👉 Points in Segments
+
 int main()
 {
-    int n;
-    cin >> n;
-    vector<int> v;
-    for (int i = 0; i < n; i++)
+    int t;
+    cin >> t;
+    vector<vector<int>> v;
+    vector<vector<pair<int, int>>> vp;
+
+    for (int i = 0; i < t; i++)
     {
-        int el;
-        cin >> el;
-        v.push_back(el);
+        int p, s;
+        cin >> p >> s;
+        vector<int> temp;
+        for (int j = 0; j < p; j++)
+        {
+            int n;
+            cin >> n;
+            temp.push_back(n);
+        }
+        v.push_back(temp);
+
+        vector<pair<int, int>> tempV;
+        for (int j = 0; j < s; j++)
+        {
+            int s1, s2;
+            cin >> s1 >> s2;
+            tempV.push_back({s1, s2});
+        }
+        vp.push_back(tempV);
     }
 
-    int query;
-    cin >> query;
-    vector<pair<int, int>> vp;
-    for (int i = 0; i < query; i++)
+    for (int i = 0; i < v.size(); i++)
     {
+        cout << "Case " << i + 1 << ": " << endl;
+        for (int j = 0; j < vp[i].size(); j++)
+        {
+            auto lbOfHigh = upper_bound(v[i].begin(), v[i].end(), vp[i][j].second);
 
-        int to_find;
-        cin >> to_find;
-        auto lb = lower_bound(v.begin(), v.end(), to_find);
-        auto hi = lower_bound(v.begin(), v.end(), to_find + 1);
+            auto lbOfLow = lower_bound(v[i].begin(), v[i].end(), vp[i][j].first);
 
-        int indexl = lb - v.begin();
-        int indexH = hi - v.begin();
-
-        if (indexl == 0)
-            vp.push_back({0, v[indexH]});
-        else if (indexH == n)
-            vp.push_back({v[indexl - 1], 0});
-        else
-            vp.push_back({v[indexl - 1], v[indexH]});
-    }
-
-    for (auto val : vp)
-    {
-        if (val.first == 0)
-            cout << "X"
-                 << " " << val.second << endl;
-        else if (val.second == 0)
-            cout << val.first
-                 << " "
-                 << "X" << endl;
-        else
-            cout << val.first << " " << val.second << endl;
+            int result = (lbOfHigh - v[i].begin()) - (lbOfLow - v[i].begin());
+            cout << result << endl;
+        }
     }
 
     return 0;
 }
+
+// //////// 👉👉👉👉👉👉👉 The Playboy Chimp
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> v;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int el;
+//         cin >> el;
+//         v.push_back(el);
+//     }
+
+//     int query;
+//     cin >> query;
+//     vector<pair<int, int>> vp;
+//     for (int i = 0; i < query; i++)
+//     {
+
+//         int to_find;
+//         cin >> to_find;
+//         auto lb = lower_bound(v.begin(), v.end(), to_find);
+//         auto hi = lower_bound(v.begin(), v.end(), to_find + 1);
+
+//         int indexl = lb - v.begin();
+//         int indexH = hi - v.begin();
+
+//         if (indexl == 0)
+//             vp.push_back({0, v[indexH]});
+//         else if (indexH == n)
+//             vp.push_back({v[indexl - 1], 0});
+//         else
+//             vp.push_back({v[indexl - 1], v[indexH]});
+//     }
+
+//     for (auto val : vp)
+//     {
+//         if (val.first == 0)
+//             cout << "X"
+//                  << " " << val.second << endl;
+//         else if (val.second == 0)
+//             cout << val.first
+//                  << " "
+//                  << "X" << endl;
+//         else
+//             cout << val.first << " " << val.second << endl;
+//     }
+
+//     return 0;
+// }
 
 // Binary search
 
